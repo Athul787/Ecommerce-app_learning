6bedfdf04de2def72c717ba3c54a0f3890750bb5
Athul787 <athulmbabu@gmail.com>
2025-03-31 15:29:37 +0530

    user auth

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 var express = require("express");
 var router = express.Router();
 const productHelper = require("../helpers/product-helpers");
+const userHelper = require("../helpers/user-helpers");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   productHelper.getAllProducts().then((products) => {
     console.log(products);
-    res.render("user/view-products", { admin: true, products });
+    res.render("user/view-products", { admin: false, products });
   });
 });
-
+router.get("/login", (req, res) => {
+  res.render("user/login");
+});
+router.get("/signup", (req, res) => {
+  res.render("user/signup");
+});
+router.post("/signup", (req, res) => {
+  userHelper.doSignup(req.body).then((response) => {
+    console.log(response);
+  });
+});
 module.exports = router;
